d4d6765d1315bb8f81995429a8202ce271a9ac49
qader qaribiyan <gharibyan.ghader@gmail.com>
2023-05-11 10:12:34 +0200

    qnet

--- qnetwork.py
+++ qnetwork.py
@@ -21,7 +21,7 @@ import torch.nn.functional as F
 is_ipython = 'inline' in matplotlib.get_backend()
 if is_ipython:
     from IPython import display
-BATCH_SIZE = 128
+BATCH_SIZE = 10
 GAMMA = 0.99
 EPS_START = 0.9
 EPS_END = 0.05
@@ -39,13 +39,27 @@ def preprocess_observation(obs):
     color = np.array([210, 164, 74]).mean()
     # Crop and resize the image
     res = cv2.resize(obs, dsize=(200, 200), interpolation=cv2.INTER_CUBIC)
+    print(res.shape)
     crop_img = res[15:190, 0:200]
+    print(crop_img.shape)
+
     # Convert the image to greyscale
-    crop_img = crop_img.mean(axis=2)
+    # crop_img = crop_img.mean(axis=2)
     # Improve image contrast
-    crop_img[crop_img == color] = 0
+    # crop_img[crop_img == color] = 0
     res = cv2.resize(crop_img, dsize=(84, 84), interpolation=cv2.INTER_CUBIC)
-    return res
+    print(res.shape)
+    # Convert image to PyTorch tensor and normalize
+    img = np.transpose(res, (2, 0, 1)).astype(np.float32) / 255.0
+    img = torch.from_numpy(img).to(device)
+    return img.unsqueeze(0)
+    # Add batch dimension
+    img = img.unsqueeze(0)
+    print(img.shape)
+    screen = torch.from_numpy(res).to(device)
+    return img.to(device)
+    # return screen
+    return torch.from_numpy(res).unsqueeze(0).unsqueeze(0).to(device)
 
 
 class DQN(nn.Module):
@@ -53,7 +67,7 @@ class DQN(nn.Module):
         super(DQN, self).__init__()
 
         self.device = device
-        self.conv1 = nn.Conv2d(1, 16, kernel_size=5, stride=2)
+        self.conv1 = nn.Conv2d(3, 16, kernel_size=5, stride=2)
         self.bn1 = nn.BatchNorm2d(16)
         self.conv2 = nn.Conv2d(16, 32, kernel_size=5, stride=2)
         self.bn2 = nn.BatchNorm2d(32)
@@ -76,10 +90,11 @@ def optimize_model(policy_DQN, target_DQN, memory, optimizer, learn_counter, dev
     if len(memory) < BATCH_SIZE:
         return learn_counter
     learn_counter += 1
-    states, actions, rewards, next_states, dones = memory.sample()
-
-    predicted_targets = policy_DQN(states).gather(1, actions)
-
+    experiences = memory.sample(BATCH_SIZE)
+    batch = Experience(*zip(*experiences))
+    state_batch = torch.cat(batch.state)
+    action_batch = torch.cat(batch.action)
+    predicted_targets = policy_DQN(state_batch).gather(1, action_batch)
     target_values = target_DQN(next_states).detach().max(1)[0]
     labels = rewards + GAMMA * (1 - dones.squeeze(1)) * target_values
 
@@ -101,8 +116,8 @@ def optimize_model(policy_DQN, target_DQN, memory, optimizer, learn_counter, dev
     return learn_counter
 
 
-policy_DQN = DQN(5).to(device)
-target_DQN = DQN(5).to(device)
+policy_DQN = DQN(4).to(device)
+target_DQN = DQN(4).to(device)
 optimizer = optim.SGD(
     policy_DQN.parameters(), lr=LR, momentum=GAMMA, nesterov=True
 )
@@ -115,13 +130,12 @@ def select_action(state, policy_DQN, learn_counter):
     eps_threshold = max(EPS_START, EPS_END - (EPS_END - EPS_START)
                         * learn_counter / EPS_DECAY)
     steps_done += 1
-    tensor = torch.tensor(state, dtype=torch.float32,
-                          device=device).unsqueeze(0).unsqueeze(0)
     with torch.no_grad():
-        q_values = policy_DQN(tensor)
+        q_values = policy_DQN(state)
     # display.data.q_values.append(q_values.max(1)[0].item())
     if sample > eps_threshold:
         # Optimal action
+        print("optimal action")
         return q_values.max(1)[1].view(1, 1)
     else:
         # Random action
@@ -140,14 +154,19 @@ class ExperienceReplay:
     def __init__(self, capacity) -> None:
         self.buffer = deque(maxlen=capacity)
 
-    def append(self, experience):
-        self.buffer.append(experience)
+    def append(self, state, action, reward, next_state, done):
+        self.buffer.append(Experience(state, action, reward, done, next_state))
 
     def sample(self, batch_size):
+        return random.sample(self.buffer, 10)
         indices = np.random.choice(len(self.buffer), batch_size, replace=False)
         states, actions, rewards, dones, next_states = zip(
             *[self.buffer[idx] for idx in indices])
-        return np.array(states), np.array(actions), np.array(rewards, dtype=np.float32), np.array(dones, dtype=np.uint8), np.array(next_states)
+        batch = Experience(*zip(*transitions))
+        return states, actions, rewards, dones, next_states
+
+    def __len__(self):
+        return len(self.buffer)
 
 
 memory = ExperienceReplay(10000)
@@ -171,17 +190,23 @@ while True:
         # if dmaker.steps_done > MAX_FRAMES:
         #     break
         # epsilon greedy decision maker
+        # actions = [0, 1, 2, 3]
+        # predicted_targets = policy_DQN(state).gather(1, torch.tensor([actions], dtype=torch.long).to(device))
+        # target_values = target_DQN(state).detach().max(1)[0]
         action = select_action(
             state, policy_DQN, learn_counter)
-        # action_ = ACTIONS[old_action][action.item()]
-        if action.item() == 0:
+        # action_ = ACTIONS[old_action][action.item()],
+        action_t = action.item()
+        if action_t == 0:
             action = UP
-        elif action.item() == 1:
+        elif action_t == 1:
             action = DOWN
-        elif action.item() == 2:
+        elif action_t == 2:
             action = LEFT
-        elif action.item() == 3:
+        elif action_t == 3:
             action = RIGHT
+        else:
+            print("ERROR")
         obs, reward_, done, remaining_lives = game.step(action)
         # display.obs = obs.copy()
         update_all = False
@@ -189,25 +214,25 @@ while True:
             lives -= 1
             jump_dead_step = True
             got_reward = False
-            reward += -100
+            reward_ += -100
             # dmaker.old_action = 3
             update_all = True
 
         if done and lives > 0:
             reward += +100
 
-        got_reward = got_reward or reward != 0
+        got_reward = reward_ != 0
         # display.data.rewards.append(reward)
-        reward = torch.tensor([reward], device=device)
+        # reward = torch.tensor([reward], device=device)
 
         old_action = action
         # if reward != 0:
         #     dmaker.old_action = action.item()
-
         next_state = preprocess_observation(obs)
-
         if got_reward:
-            memory.push(state, action, reward, next_state, done)
+            action_tensor = torch.tensor([action_t], device=device, dtype=torch.long)
+            memory.append(state,action_tensor,
+                          reward_, next_state, done)
 
         state = next_state
         learn_counter = optimize_model(
@@ -218,6 +243,8 @@ while True:
             learn_counter,
             device,
         )
+            # predicted_targets = policy_DQN(
+            #     state_batch[0])
 
         if steps_done % TARGET_UPDATE == 0:
             target_DQN.load_state_dict(policy_DQN.state_dict())
